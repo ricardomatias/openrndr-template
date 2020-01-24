8cab852fc7dcf7cbfaaca0ac3ef3f2026b61c6cf
Ricardo Matias <ricardo.matias0@gmail.com>
2020-01-24 16:31:23 +0100

    wip

--- src/main/kotlin/Sketch.kt
+++ src/main/kotlin/Sketch.kt
@@ -1,12 +1,13 @@
 import org.openrndr.Program
 import org.openrndr.application
-import org.openrndr.configuration
 import org.openrndr.extra.olive.Olive
 import org.openrndr.extra.olive.Resources
+import org.openrndr.extra.palette.PaletteStudio
 import org.openrndr.math.Vector2
 import org.openrndr.plugins.fps.FPS
 
 val olive = Olive<Program>(Resources(listOf(".yaml")))
+val paletteStudio = PaletteStudio()
 
 const val WIDTH = 768
 const val HEIGHT = 768
@@ -17,19 +18,15 @@ const val h2 = h / 2.0
 
 val origin = Vector2(200.0, 0.0)
 
-
-class CompositorSketch : Program() {
-    override fun setup() {
+fun main() = application {
+    configure {
+        width = WIDTH + origin.x.toInt()
+        height = HEIGHT
+    }
+    program {
         extend(FPS()) {
             corner = FPS.Corner.BOTTOM_LEFT
         }
         extend(olive)
     }
 }
-
-val configuration = configuration {
-    width = WIDTH + origin.x.toInt()
-    height = HEIGHT
-}
-
-fun main() = application(CompositorSketch(), configuration)
--- src/main/kotlin/live.kts
+++ src/main/kotlin/live.kts
@@ -1,12 +1,28 @@
 @file:Suppress("UNUSED_LAMBDA_EXPRESSION")
+import org.openrndr.Program
+import org.openrndr.color.ColorRGBa
+import org.openrndr.draw.colorBuffer
 import org.openrndr.draw.loadImage
+import org.openrndr.draw.renderTarget
+import org.openrndr.extensions.Screenshots
 import org.openrndr.extra.compositor.*
 import org.openrndr.extra.olive.Reloadable
+import org.openrndr.math.Vector2
 import org.openrndr.plugins.gui.*
+import org.openrndr.shape.Rectangle
 import kotlin.math.roundToInt
 
-{ program: CompositorSketch ->
+{ program: Program ->
     program.apply {
+        val rt = renderTarget(WIDTH, HEIGHT, contentScale = 2.0) {
+            colorBuffer()
+            depthBuffer()
+        }
+
+        val canvas = renderTarget(WIDTH, HEIGHT, contentScale = 2.0) {
+            colorBuffer()
+        }
+
         val params = object : Reloadable() {
             @GuiIntParam("Int", 1, 10, 0)
             var intVal = 2
@@ -22,20 +38,36 @@ import kotlin.math.roundToInt
 
         shadow.download()
 
+        data class Pixel(val rect: Rectangle, val size: Double, val color: ColorRGBa)
+
+        val pixels = mutableListOf<Pixel>()
+
+        for (x in 0 until 76) {
+            for (y in 0 until 57) {
+                val color = shadow[((x / 76.0) * image.effectiveWidth).roundToInt(), ((y / 57.0) * image.effectiveHeight).roundToInt()]
+                val rectangle = Rectangle(Vector2(origin.x + x * 10.0, origin.y + y * 10.0), 10.0, 10.0)
+                val pixel = Pixel(rectangle, 10.0, color)
+                pixels.add(pixel)
+            }
+        }
+
         val drawing = compose {
             draw {
-                for (x in 0 until 76) {
-                    for (y in 0 until 57) {
-                        drawer.stroke = null
-                        drawer.fill = shadow[((x / 76.0) * image.effectiveWidth).roundToInt(), ((y / 57.0) * image.effectiveHeight).roundToInt()]
-                        drawer.rectangle(origin.x + x * 10.0, origin.y + y * 10.0, 10.0, 10.0)
-                    }
+                drawer.stroke = null
+
+                for (pixel in pixels) {
+                    drawer.fill = pixel.color
+                    drawer.rectangles(pixel.rect)
                 }
             }
         }
 
+        extend(paletteStudio)
         extend(gui) {
             add(params)
+        }
+        extend(Screenshots()) {
+            scale = 2.0
         }
         extend {
             drawing.draw(drawer)
